5e6a08ae280d8b1bb21f54ef815680cb1cf01ad6
Gabriel Demes <gabrieldemes@nyu.edu>
2020-11-26 20:23:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
-first_variable = 1
-second_variable = 3
-sum = (first_variable + second_variable)
-difference = (first_variable - second_variable)
-quotient = (firat_variable / secons_variable)
-product = (first variable * second variable)
+first_number = 1
+second_number = 3
+sum = (first_number + second_number)
+difference = (first_number - second_number)
+quotient = (first_number / second_number)
+product = (first_number * second_number)
